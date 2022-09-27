d12990d38f5bb3b9e3f2d1dd09df188876739413
gabriel <gabriel@okteto.com>
2022-09-27 12:45:52 +0200

    openid sample for azure

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.send('online');
 })
 
+app.get('/login', function (req, res) {
+  res.send('login ok');
+})
+
+app.get('/logout', function (req, res) {
+  res.send('logout ok');
+})
+
+
 app.listen(3000, function () {
-  console.log('Starting hello-world server...');
+  console.log('openid started');
 })
