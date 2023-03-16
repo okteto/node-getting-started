6c0829220f60200d53520bd2b7f8ebd9bf2e44b6
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2023-03-16 14:59:50 +0100

    Clarify log message that server is started
    Address confusion on community question, https://community.okteto.com/t/in-development-app-doesnt-work/725/3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello world!');
 })
 
-app.listen(3000, function () {
-  console.log('Starting hello-world server...');
+app.listen(PORT, function () {
+  console.log('Started hello-world server on %d', PORT);
 })
